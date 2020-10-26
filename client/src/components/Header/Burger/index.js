3e3358b4abcd4d59f8c2b16aa2492c453786b800
AntoineDecamps <antoine.ram16@gmail.com>
2020-10-26 10:33:57 +0100

    import of react-responsive dependencies + testing

---
+++ client/src/components/Header/Burger/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './styles.scss';
+
+const Burger = () => (
+  <div className="burger">Burger</div>
+);
+
+Burger.propTypes = {};
+
+export default Burger;
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import PropTypes from 'prop-types';
 
 import './styles.scss';
 
-const Header = () => (
-  <div className="header">Header</div>
-);
+const Header = () => {
+  const isBigScreen = useMediaQuery({ query: '(min-device-width: 1824px)' });
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1024px)' });
+  return (
+    <>
+      {isBigScreen && (
+        <p>DESKTOP</p>
+      )}
+      {isTabletOrMobile && (
+        <p>MOBILE</p>
+      )}
+      <div className="header">Header</div>
+    </>
+  );
+};
 
 Header.propTypes = {};
 
