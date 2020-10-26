07174b2f904a23cbc70496b67f04f2b7ee3cc23d
AntoineDecamps <antoine.ram16@gmail.com>
2020-10-26 08:38:54 +0100

    initialisation of the project with a React modele in the client directory

---
+++ client/src/components/App/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './styles.css';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <h1>VB Project</h1>
+  </div>
+);
+
+// == Export
+export default App;
