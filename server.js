628724a13ee10c5151577045799c70ffaf41aa15
AntoineDecamps <antoine.ram16@gmail.com>
2021-02-09 11:22:56 +0100

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const dbMiddleware = require('./middlewares/mongodb.js');
+// const dbMiddleware = require('./middlewares/mongodb.js');
 
 
 const authRouter = require('./routes/auth.js');
